d502dc6f274e51840e42c26c27662fb783ab425a
TomP <tomp12@protonmail.com>
2024-08-16 22:38:56 +0300

    dev

--- pkg/nodes/factory/node_factory.go
+++ pkg/nodes/factory/node_factory.go
@@ -1,6 +1,7 @@
 package factory
 
 import (
+	"context"
 	"errors"
 	"github.com/rs/zerolog/log"
 	"github.com/tomp332/p2p-agent/pkg/nodes"
@@ -11,7 +12,7 @@ import (
 	"reflect"
 )
 
-func InitializeNodes(server *server.GRPCServer, nodeConfigs *configs.AllNodesMap) ([]nodes.P2PNoder, error) {
+func InitializeNodes(ctx context.Context, server *server.GRPCServer, nodeConfigs *configs.AllNodesMap) ([]nodes.P2PNoder, error) {
 	var initializedNodes []nodes.P2PNoder
 	val := reflect.ValueOf(nodeConfigs)
 	// Dereference the pointer to access the struct
@@ -20,29 +21,41 @@ func InitializeNodes(server *server.GRPCServer, nodeConfigs *configs.AllNodesMap
 	}
 	// Get the number of fields in the struct
 	numFields := val.NumField()
+
 	for i := 0; i < numFields; i++ {
+		// Check if the context has been canceled
+		select {
+		case <-ctx.Done():
+			log.Warn().Msg("Initialization cancelled by context")
+			return initializedNodes, ctx.Err()
+		default:
+			// Continue initialization if context is still active
+		}
+
 		field := val.Type().Field(i)
 		value := val.Field(i)
 		nodeConfig := value.Interface().(*configs.NodeConfig)
 		if nodeConfig == nil {
 			continue
 		}
+
 		nodeConfig.Type = configs.StrToNodeType(field.Name)
-		if fileNode, err := InitializeNode(server, nodeConfig); err != nil {
-			log.Error().Err(err).Str("nodeType", nodeConfigs.FileNode.Type.ToString()).Msgf("Failed to initialize nodes")
+		if fileNode, err := InitializeNode(ctx, server, nodeConfig); err != nil {
+			log.Error().Err(err).Str("nodeType", nodeConfig.Type.ToString()).Msgf("Failed to initialize node")
 		} else {
 			initializedNodes = append(initializedNodes, fileNode)
 		}
 	}
+
 	if initializedNodes == nil {
 		return nil, errors.New("no nodes have been configured")
 	}
 	return initializedNodes, nil
 }
 
-func InitializeNode(server *server.GRPCServer, config *configs.NodeConfig) (nodes.P2PNoder, error) {
+func InitializeNode(mainCtx context.Context, server *server.GRPCServer, config *configs.NodeConfig) (nodes.P2PNoder, error) {
 	var n nodes.P2PNoder
-	baseNode := nodes.NewBaseNode(config)
+	baseNode := nodes.NewBaseNode(config, mainCtx)
 	switch config.Type {
 	case configs.FilesNodeType:
 		if config.Storage.RootDirectory == "" {
@@ -55,10 +68,7 @@ func InitializeNode(server *server.GRPCServer, config *configs.NodeConfig) (node
 	}
 	n.InterceptorsRegister(server)
 	n.ServiceRegister(server)
-	err := n.ConnectToBootstrapPeers()
-	if err != nil {
-		log.Warn().Msgf("Bootstrapping process failed")
-	}
+	n.ConnectToBootstrapPeers()
 	options := n.Options()
 	log.Info().
 		Str("id", configs.MainConfig.ID).
